1fa868b83326ddbe0c756bc61b7abd626a54b426
sgigli <simon.luciano@gigli.net>
2021-08-14 19:29:30 -0400

    remove explicit user form from first implementation

--- users/views.py
+++ users/views.py
@@ -7,19 +7,3 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('users:login')
     template_name = 'registration/signup.html'
-
-
-
-
-
-# from django.urls import reverse_lazy
-# from .forms import UserRegisterForm
-# from django.contrib.messages.views import SuccessMessageMixin
-# from django.views.generic.edit import CreateView
-
-# class SignUpView(SuccessMessageMixin, CreateView):
-#   template_name = 'users/register.html'
-#   success_url = reverse_lazy('login')
-#   form_class = UserRegisterForm
-#   success_message = "Your profile was created successfully"
-
